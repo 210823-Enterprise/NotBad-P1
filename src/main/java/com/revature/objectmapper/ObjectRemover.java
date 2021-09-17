99fb3706c4049ba30580708f63b3153b52f48f9d
ImCoolYeah105 <maw23456789@gmail.com>
2021-09-17 13:42:13 -0400

    update maven

--- src/main/java/com/revature/objectmapper/ObjectRemover.java
+++ src/main/java/com/revature/objectmapper/ObjectRemover.java
@@ -1,6 +1,5 @@
 package com.revature.objectmapper;
 
-import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.ParameterMetaData;
 import java.sql.PreparedStatement;
@@ -9,16 +8,16 @@ import com.revature.util.MetaModel;
 
 public class ObjectRemover extends ObjectMapper{
 	
-	public boolean removeObjectFromDb(Object obj, Connection conn) {
+	public boolean removeObjectFromDb(final Object obj, final Connection conn) {
 		
-		MetaModel<?> model = MetaModel.of(obj.getClass()); // use this to creaet an instance of the object
+		final MetaModel<?> model = MetaModel.of(obj.getClass()); // use this to creaet an instance of the object
 		
 		
-		String primaryKey = model.getPrimaryKey().getName(); // change this to IdField
-		String sql 		  = "DELETE from " + model.getSimpleClassName() + " where " + primaryKey + "= ?"; // create some type of method that returns the table name in MetaModel;
-		PreparedStatement pstmt =  conn.prepareStatement(sql);
+		final String primaryKey = model.getPrimaryKey().getName(); // change this to IdField
+		final String sql 		  = "DELETE from " + model.getSimpleClassName() + " where " + primaryKey + "= ?"; // create some type of method that returns the table name in MetaModel;
+		final PreparedStatement pstmt =  conn.prepareStatement(sql);
 		// we want to grab meta data from this statement
-		ParameterMetaData pd = pstmt.getParameterMetaData();
+		final ParameterMetaData pd = pstmt.getParameterMetaData();
 		
 		// instead of Method, maybe pass in a hashmap containing info about the object that you
 		setStatement(pstmt, pd, obj, 1);
