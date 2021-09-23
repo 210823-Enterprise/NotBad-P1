130f6aca0c659ae1bbf683a01cfc24fea4f3fc2e
ImCoolYeah105 <maw23456789@gmail.com>
2021-09-23 18:54:26 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 cd ORM
-mvn clean package
+mvn package
 mvn install:install-file -Dfile=target/NotBadORM-0.0.1-SNAPSHOT.jar -DgroupId=com.revature -DartifactId=NotBadORM -Dversion=0.0.1 -Dpackaging=jar -DgeneratePom=true
 cd ..
 cp ORM/target/NotBadORM-0.0.1-SNAPSHOT.jar WebApp/src/main/resources
 cd WebApp
-mvn clean package
+mvn package
