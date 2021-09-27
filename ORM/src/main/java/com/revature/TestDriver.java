ab1ad00510b09c3e0173d71c877c67e97825660e
ImCoolYeah105 <maw23456789@gmail.com>
2021-09-27 12:20:29 -0400

    fix game

--- ORM/src/main/java/com/revature/TestDriver.java
+++ ORM/src/main/java/com/revature/TestDriver.java
@@ -8,23 +8,36 @@ import com.revature.orm.ORM;
 public class TestDriver {
 
 	public static void main(final String[] args) {
+		/*
+		 * Load objects into the configuration and run finalize so the
+		 * tables are generated.
+		 */
 		final Configuration config = Configuration.getInstance();
 		config.addAnnotatedClass(TestClass.class);
 		config.addAnnotatedClass(SubTestClass.class);
 		config.finalizeConfig();
 		
+		//testing class
 		final TestClass testClass = new TestClass(1,"name","pass", new SubTestClass(1,"subclass"));
-		assert ORM.getInstance().addObjectToDb(testClass) : "Failed to add object to database";
+		final ORM orm = ORM.getInstance();
 		
+		//attempt to add item to database
+		assert orm.addObjectToDb(testClass) : "Failed to add object to database";
+		
+		//change the username and password, then update the database
 		testClass.setTestUsername("newUsername");
 		testClass.getSubclass().setTestUsername("newUsername");
-		assert ORM.getInstance().updateObjectInDb(testClass) : "Failed to update object in database";
+		assert orm.updateObjectInDb(testClass) : "Failed to update object in database";
 		
-		final TestClass returned = ORM.getInstance().getObjectFromDb(TestClass.class,testClass.getId());
+		//get object and verfiy it's the same one
+		final TestClass returned = orm.getObjectFromDb(TestClass.class,testClass.getId());
 		assert returned.getId() == testClass.getId() : "Failed to get object from database";
 		assert returned.getSubclass().getId() == testClass.getSubclass().getId() : "Failed to get sub-object from database";
 		
-		assert ORM.getInstance().removeObjectFromDb(testClass) : "Failed to remove object from database";
+		//delete object from db
+		assert orm.removeObjectFromDb(testClass) : "Failed to remove object from database";
+		
+		System.out.println("All tests passed.");
 	}
 
 }
